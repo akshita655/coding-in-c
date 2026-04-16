fb7088355aeb6b29f08655ec00b4b18109bd42e7
Akshita Dhiman <akshitadhiman655@gmail.com>
2026-04-17 00:32:05 +0530

    Add files via upload

---
+++ Binary_Tree/right_view.cpp
@@ -0,0 +1,71 @@
+//RIGHT VIEW OF A BINARY TREE
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<vector>
+using namespace std;
+
+struct node * create_node();
+void left(struct node * root);
+
+struct node{
+	int data;
+	struct node * left;
+	struct node * right;
+};
+
+struct node * create_node(){
+	int value;
+	struct node * newnode;
+	newnode = (struct node *)malloc(sizeof(struct node));
+	printf("Enter the data(-1 for no node): ");
+	scanf("%d",&value);
+	if(value == -1){
+		return NULL;
+	}
+	newnode -> data = value;
+	printf("Enter Left Child of %d ",value);
+	newnode -> left = create_node();
+	printf("Enter Right Child of %d ",value);
+	newnode -> right = create_node();
+	return newnode;
+}
+
+int height(struct node * root){
+	if(root == NULL){
+		return 0;
+	}
+	int l = height(root -> left);
+	int r = height(root -> right);
+	return 1 + (l > r ? l : r);
+}
+
+void level(struct node * root, int i, vector<int> &result){
+	if(root == NULL){
+		return;
+	}
+	if(i == 0){
+		result.push_back(root -> data);
+		return;
+	}
+	level(root -> left, i - 1, result);
+	level(root -> right, i - 1, result);
+}
+
+void right(struct node * root){
+	int i, h = height(root);
+	for(i=0;i<h;i++){
+		vector<int> result;
+		level(root, i, result);
+		if(!result.empty()){
+			printf("%d ",result[result.size() - 1]);
+		}
+	}
+}
+
+int main(){
+    struct node *root = create_node();
+    printf("\nRight View: ");
+    right(root);
+    return 0;
+}
