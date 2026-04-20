7cad624697ff6e6f099d270e58bb6950d45c40ce
Akshita Dhiman <akshitadhiman655@gmail.com>
2026-04-20 16:08:56 +0530

    Add files via upload

---
+++ Binary_Search_Tree/max_in_BST.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node * create_node(struct node *root, int data);
+int find_max(struct node * root);
+
+struct node{
+	int data;
+	struct node * left;
+	struct node * right;
+}*root = NULL;
+
+int main() {
+	int choice = 1, data;
+	while(choice == 1){
+		printf("Enter data to insert: ");
+        scanf("%d", &data);
+        root = create_node(root, data);
+        printf("Want to enter more data?(0, 1) ");
+        scanf("%d",&choice);
+	}
+	printf("Max Element: %d",find_max(root));
+}
+
+struct node* create_node(struct node *root, int data) {
+    struct node *newnode;
+    if (root == NULL) {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->left = NULL;
+        newnode->data = data;
+        newnode->right = NULL;
+        return newnode;
+    } else if (data < root->data) {
+        root->left = create_node(root->left, data);
+    } else {
+        root->right = create_node(root->right, data);
+    }
+    return root;
+}
+
+int find_max(struct node * root){
+	if(root == NULL){
+		return -1;
+	}
+	if(root -> right == NULL){
+		return root -> data;
+	}
+	return find_max(root -> right);
+}
