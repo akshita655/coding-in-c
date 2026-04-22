816d3118c1a5bd6b2a4550789219363d7753d82c
Akshita Dhiman <akshitadhiman655@gmail.com>
2026-04-22 19:54:44 +0530

    Add files via upload

---
+++ Binary_Search_Tree/count_nodes_BST.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node * create_node(struct node *root, int data);
+int count(struct node * root);
+
+struct node{
+	int data;
+	struct node * left;
+	struct node * right;
+}*root = NULL;
+
+int main() {
+	int choice = 1, data;
+	while(choice == 1){
+		printf("Enter data to insert: ");
+        scanf("%d", &data);
+        root = create_node(root, data);
+        printf("Want to enter more data?(0, 1) ");
+        scanf("%d",&choice);
+	}
+	printf("Total Number of Nodes in Tree: %d",count(root));
+}
+
+struct node* create_node(struct node *root, int data) {
+    struct node *newnode;
+    if (root == NULL) {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->left = NULL;
+        newnode->data = data;
+        newnode->right = NULL;
+        return newnode;
+    } else if (data < root->data) {
+        root->left = create_node(root->left, data);
+    } else {
+        root->right = create_node(root->right, data);
+    }
+    return root;
+}
+
+int count(struct node * root){
+	if(root == NULL){
+		return 0;
+	}
+	int l = count(root -> left);
+	int r = count(root -> right);
+	return 1 + l + r;
+}
